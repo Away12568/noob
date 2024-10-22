a4f8b4458b504ed2ad88e7adc9ac59a58b925529
awat <154649552+Away12568@users.noreply.github.com>
2024-10-22 18:52:48 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header/header'
+import Hero from './components/hero/hero'
+import Footer from './components/Footer/Footer'
+
+
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+        <Hero/>
+      <Footer/>
+
+    </>
+  )
+}
+
+export default App
